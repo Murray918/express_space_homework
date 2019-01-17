6da31aeb86718aad8b4653e1bc1fbcb3ff8ab730
Jacob Werbin <jwerbin95@gmail.com>
2019-01-17 11:27:43 -0600

    Removed unecessary comments

--- server.js
+++ server.js
@@ -3,49 +3,31 @@ const express = require('express');
 const app = express();
 let ejs = require('ejs');
 let marsMissions = require('./models/marsMissions.js');
-// run `npm install` to install dependencies in package.json
-
-// * Your mission is to complete the app
-// * The app will need routes for index and show
-// * The app will need views for index and show
-//
-// * MAIN GOAL:
-// * User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page
-//
-// * Bonus/Hungry for More: add images to the data and have them display (google how)
-// * Bonus/Hungry for More: add static css to style the pages (google how)
-
-// NOTES:
-// ejs has not been installed
-// views folder has not been created
-// views/missions folder has not been created
 
 // PORT
 const port = 3000;
 
 // INDEX Route
-// send data to 'missions/index.ejs' view
-// the view should display just the names of each mission
-// display the mission names as <li> in a <ul> with the class name "missions"
-
-// SHOW Route
-// send data to 'missions/show.ejs' view
-// the view should display all the data for a single mission
+// the view displays just the names of each mission
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/missions', function(request, response) {
-  response.render('missions/index.ejs', {
-    marsMissions: marsMissions
-  });
+	response.render('missions/index.ejs', {
+		marsMissions: marsMissions
+	});
 });
+
+// SHOW Route
+// the view displays all the data for a single mission
 app.get('/missions/:index', function(request, response) {
-  response.render('missions/show.ejs', {
-    marsMissions: marsMissions[request.params.index]
-  });
+	response.render('missions/show.ejs', {
+		marsMissions: marsMissions[request.params.index]
+	});
 });
+
 // LISTENER
 app.listen(port, function() {
-  console.log('Missions to Mars running on port: ', port);
+	console.log('Missions to Mars running on port: ', port);
 });
 
 module.exports = app;
