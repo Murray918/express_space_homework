f5a045681d71c18c403da6f0789d1f9d176fd4a2
Sithiz <Joshklaas1@gmail.com>
2019-01-17 16:11:55 -0600

    COMplEtEd SPAcE

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 // DEPENDENCIES
 const express = require('express');
-const app = express();
+const marsMissions = require('./models/marsMissions.js')
 
+
+let app = express();
+
+app.set('view engine','ejs')
+
+
+
+
+
+
+console.log(marsMissions)
 // run `npm install` to install dependencies in package.json
 
 // * Your mission is to complete the app
@@ -20,57 +31,47 @@ const app = express();
 // views/missions folder has not been created
 
 // PORT
-const port = 3000;
+const port = 9000;
 
 // DATA - put into marsMissions.js file inside of a models folder, for module.exports
 // remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
+
+
+
+
+
+
+
+
+
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
 
+
+
+
+app.get('/',(request,response)=>{
+  response.render('missions/index.ejs', { 
+  	thing: marsMissions,
+  	page: 'list.ejs',
+
+  })
+})
+
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
 
+app.get('/show/:id',(request,response)=>{
+	response.render('missions/index.ejs',{
+		thing: marsMissions[request.params.id],
+		page: 'show.ejs',
+
+	})
+})
 
 
 // LISTENER
@@ -79,3 +80,4 @@ app.listen(port, function() {
 })
 
 module.exports = app;
+
